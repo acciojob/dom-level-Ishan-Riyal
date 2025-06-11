641c60c2008f754c92f1d5999f991d4189b62635
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-11 18:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+
+let level = 0;
+let currentElement = element;
+
+// Traverse up the DOM tree until reaching null (above the <html> root)
+while (currentElement) {
+    level++;
+    currentElement = currentElement.parentElement;
+}
+
+alert("The level of the element is: " + level);
